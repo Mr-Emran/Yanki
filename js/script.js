f08cc5f402ffb4c85be6e6ef796e8173d24967ad
Mr-Emran <126672039+Mr-Emran@users.noreply.github.com>
2023-03-10 16:21:24 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,12 +14,15 @@ let doc = document
 let menu_btn = doc.querySelector('.burger-menu')
 let menu_activ = doc.querySelector('.menu_activ')
 let menu_btn2 = doc.querySelector('.bttn')
+let body = doc.querySelector('body')
 
 let change = () =>{
 menu_activ.style = "transform: translateY(0)"
+body.style.overflow = 'hidden'
 }
 let change2 = () =>{
     menu_activ.style = "transform: translateY(-300px)"
+    body.style.overflow = 'auto'
 }
 
 menu_btn.addEventListener('click',change)
